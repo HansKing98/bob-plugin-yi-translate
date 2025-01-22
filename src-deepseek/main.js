b5373caa570f0c1e78c864b72bc35b4effe5b21a
hans <im.hansking@gmail.com>
2025-01-22 17:58:33 +0800

    fix:  add deepseek

---
+++ src-deepseek/main.js
@@ -0,0 +1,103 @@
+var config = require('./config.js');
+var utils = require('./utils.js');
+var deepseek = require('./deepseek.js');
+var file = require("./file.js");
+const { readFile } = require("./file.js");
+
+// var { historyFileName, readFile, writeFile } = require("./file");
+// 入参格式:
+// {
+//     "model": "yi-34b-chat-0205",
+//     "messages": [{"role": "user", "content": "帮我把Banana翻译为中文"}]
+// }
+// 出参格式:
+// {"id":"0f1a1078b531435a9ffc29eb69d403e9","object":"chat.completion","created":1678332271,"model":"yi-34b-chat-0205","usage":{"prompt_tokens":24,"completion_tokens":27,"total_tokens":67},"choices":[{"message":{"role":"assistant","content":"香蕉"},"finish_reason":"stop","index":0}]}
+
+
+function supportLanguages() {
+    return config.supportedLanguages.map(([standardLang]) => standardLang);
+}
+
+function translate(query, completion) {
+    (async () => {
+        const targetLanguage = utils.langMap.get(query.detectTo);
+        const sourceLanguage = utils.langMap.get(query.detectFrom);
+        if (!targetLanguage) {
+            const err = new Error();
+            Object.assign(err, {
+                _type: 'unsupportLanguage',
+                _message: '不支持该语种',
+            });
+            throw err;
+        }
+        const source_lang = sourceLanguage || 'ZH';
+        const target_lang = targetLanguage || 'EN';
+        const translate_text = query.text || '';
+        if (translate_text !== '') {
+            // 触发指令的结果
+            const directiveResult = utils.getDirectiveResult(translate_text);
+            if (directiveResult) {
+                completion({
+                    result: {
+                        from: query.detectFrom,
+                        to: query.detectTo,
+                        toParagraphs: directiveResult.split('\n'),
+                    },
+                });
+
+                return;
+            }
+
+
+            // 获取对话结果
+            let chatResult = ''
+
+            try {
+                const server = $option.service;
+                $log.info('hans')
+                $log.info(server)
+                $log.info('hans')
+                if (server === 'api_key_1') {
+                    chatResult = await deepseek.translate(query, source_lang, target_lang, translate_text, completion)
+                } else {
+                    // 默认请求 api.deepseek.com
+                    chatResult = await deepseek.translate(query, source_lang, target_lang, translate_text, completion)
+                    return;
+                }
+                let mode = $option.mode;
+                const configValue = readFile();
+                if (configValue.mode) {
+                    mode = configValue.mode;
+                }
+                // 对话模式就保存
+                if (mode === 'conversation') {
+                    message.push({
+                        content: chatResult,
+                        role: "assistant",
+                    });
+                    file.writeFile({
+                        value: message,
+                        fileName: file.historyFileName,
+                    });
+                }
+            } catch (e) {
+                Object.assign(e, {
+                    _type: 'network',
+                    _message: '接口请求错误 - ' + JSON.stringify(e),
+                });
+                throw e;
+            }
+        }
+    })().catch((err) => {
+        completion({
+            error: {
+                type: err._type || 'unknown',
+                message: err._message || '未知错误',
+                addtion: err._addtion,
+            },
+        });
+    });
+}
+
+exports.supportLanguages = supportLanguages;
+exports.translate = translate;
---
+++ src-deepseek/utils.js
@@ -0,0 +1,68 @@
+var config = require('./config.js');
+var { configFileName, readFile, writeFile, deleteFile, deleteAllFile } = require("./file.js");
+
+const langMap = new Map(config.supportedLanguages);
+const langMapReverse = new Map(config.supportedLanguages.map(([standardLang, lang]) => [lang, standardLang]));
+
+function getDirectiveResult(text) {
+    const configValue = readFile();
+
+    let message;
+
+    switch (text) {
+        case "#mode":
+        case "#Mode":
+        case "#模式":
+            if (configValue.mode === "conversation") {
+                return "当前处于对话模式，有什么可以帮助你的呢？";
+            } else if (configValue.mode === "polishing") {
+                return "当前处于文字润色模式，有什么可以帮助你的呢？";
+            } else if (configValue.mode === "custom_prompt") {
+                return "当前处于自定义prompt模式，有什么可以帮助你的呢？";
+            } else {
+                return "当前处于翻译模式，我支持很多种语言翻译哦~";
+            }
+
+        case "#switch":
+        case "#Switch":
+        case "#切换":
+            if (configValue.mode === "conversation") {
+                configValue.mode = "translate";
+                message = "已切换到翻译模式！";
+            } else if (configValue.mode === "translate") {
+                configValue.mode = "polishing";
+                message = "已切换到润色文字模式！";
+            } else if (configValue.mode === "polishing") {
+                configValue.mode = "custom_prompt";
+                message = "已切换到自定义prompt模式！";
+            } else {
+                configValue.mode = "conversation";
+                message = "已切换到对话模式！";
+            }
+            break;
+
+        case "#clear":
+        case "#Clear":
+        case "#清空":
+            deleteFile();
+
+            return "已清除对话记录，你可以继续聊天。";
+        case "#重置":
+        case "#reset":
+        case "#Reset":
+            deleteAllFile();
+            return "已重置所有设置，你可以继续聊天。";
+    }
+
+    writeFile({
+        value: configValue,
+        fileName: configFileName,
+    });
+
+    return message;
+}
+
+
+exports.langMap = langMap;
+exports.langMapReverse = langMapReverse;
+exports.getDirectiveResult = getDirectiveResult;
